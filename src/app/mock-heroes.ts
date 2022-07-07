e9ce05729ef969acd13668de8cdfacacc66a1dc3
b0cchi <b0cchi@outlook.jp>
2022-07-07 15:11:20 +0900

    done step 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: '竈門 禰豆子' },
+  { id: 13, name: '我妻 善逸' },
+  { id: 14, name: '嘴平 伊之助' },
+  { id: 15, name: '冨岡 義勇' },
+  { id: 16, name: '胡蝶 しのぶ' },
+  { id: 17, name: '煉󠄁獄 杏寿郎' },
+  { id: 18, name: '宇髄 天元' },
+  { id: 19, name: '時透 無一郎' },
+  { id: 20, name: '甘露寺 蜜璃' },
+];
