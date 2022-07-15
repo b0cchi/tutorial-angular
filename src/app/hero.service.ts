aae555f6e8d5c7a7e69848ccd5c048892bd85a74
b0cchi <b0cchi@outlook.jp>
2022-07-15 16:54:50 +0900

    end 5 step

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,14 +5,21 @@ import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+    constructor(private messageService: MessageService) {}
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-  }
+    getHeroes(): Observable<Hero[]> {
+        const heroes = of(HEROES);
+        this.messageService.add('HeroService: fetched heroes');
+        return heroes;
+    }
+
+    getHero(id: number): Observable<Hero> {
+        console.log(id);
+        const hero = HEROES.find((h) => h.id === id);
+        this.messageService.add(`HeroService: fetched hero id=${id}`);
+        return of(hero!);
+    }
 }
